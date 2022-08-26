51ba9cf82d8a91dee8d9b5955550cd2ce80c3368
Sandro Bistene <sandrobistene@Sandros-MacBook-Pro.local>
2022-08-25 23:07:40 -0300

    req 1

--- src/database/migrations/20220825224147-create-users.js
+++ src/database/migrations/20220825224147-create-users.js
@@ -2,20 +2,33 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      displayName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      }
+    });
   },
-
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('Users');
   }
 };
--- src/database/migrations/20220825224413-create-blog-posts.js
+++ src/database/migrations/20220825224413-create-blog-posts.js
@@ -1,21 +1,39 @@
 'use strict';
-
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
-
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
-};
+ module.exports = {
+   up: async (queryInterface, Sequelize) => {
+     await queryInterface.createTable('BlogPosts', {
+       id: {
+         allowNull: false,
+         autoIncrement: true,
+         primaryKey: true,
+         type: Sequelize.INTEGER
+       },
+       title: {
+         type: Sequelize.STRING,
+         allowNull: false,
+       },
+       content: {
+         type: Sequelize.STRING,
+         allowNull: false,
+       },
+       userId: {
+         type: Sequelize.INTEGER,
+         references: {
+           model: "Users",
+           key: "id",
+         },
+       },
+       published: {
+         type: Sequelize.DATE,
+         allowNull: false,
+       },
+       updated: {
+         type: Sequelize.DATE,
+         allowNull: false,
+       }
+     });
+   },
+   down: async (queryInterface, Sequelize) => {
+     await queryInterface.dropTable('BlogPosts');
+   }
+ };
