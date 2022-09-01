a3f48ad0ce6baa6b8285fa5ceb3eb3795ee6ae31
Sandro Bistene <sandrobistene@Sandros-MacBook-Pro.local>
2022-09-01 19:34:10 -0300

    11

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+    const PostCategory = sequelize.define('PostCategory',
+      {
+        postId: {
+          type: DataTypes.INTEGER,
+          foreignKey: true,
+          primaryKey: true,
+        },
+        categoryId: {
+          type: DataTypes.INTEGER,
+          foreignKey: true,
+          primaryKey: true,
+        },
+      },
+      { timestamps: false },
+    );
+ 
+    PostCategory.associate = (models) => {
+      models.BlogPost.belongsToMany(models.Category, {
+        as: 'categories',
+        through: PostCategory,
+        foreignKey: 'postId',
+        otherKey: 'categoryId',
+      });
+      models.Category.belongsToMany(models.BlogPost, {
+        as: 'BlogPost',
+        through: PostCategory,
+        foreignKey: 'categoryId',
+        otherKey: 'postId',
+      });
+    };
+ 
+    return PostCategory;
+  };
